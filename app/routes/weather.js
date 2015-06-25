615d0696b82bb4772b27bc15e762eca9249e234c
Matt Chastain <matthewdchastain@gmail.com>
2015-06-25 16:52:28 -0400

    Index styles done; geolocation obtained on index pageload; weather fetched and console.log'd on weather route page load

---
+++ app/routes/weather.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({	
+	model: function(params) {
+		return Ember.$.ajax('https://api.forecast.io/forecast/d635250544a43a8728090f437d22333f/'+params.lat+','+params.lng, 
+			{dataType: 'jsonp'}).then(function(response) {
+				console.log(response);
+			});
+	}
+});
