08b3a422dff1d3e4a87815b70c690f14beb87a53
Matt Chastain <matthewdchastain@gmail.com>
2015-06-26 11:26:29 -0400

    Added styles for various temperatures; today's weather displaying without variations of icon; tomorrow template completed

--- app/components/weather-results.js
+++ app/components/weather-results.js
@@ -9,6 +9,20 @@ export default Ember.Component.extend({
 		var temp = this.get('results.currently.temperature');
 		temp = temp.toString().slice(0,2);
 		this.set('results.currently.temperature', temp);
-	}
+		var todayMaxTemp = this.get('results.daily.data.0.temperatureMax');
+		// console.log(todayMaxTemp);
+	},
+
+	currentTemp: function() {
+		return this.get('results.currently.temperature').toString().slice(0,2);
+	}.property('results.@each'),
+
+	todayMaxTemp: function () {
+		return this.get('results.daily.data.0.temperatureMax').toString().slice(0,2);
+	}.property('results.@each'),
+
+	tomorrowConditions: function() {
+		return this.get('results.daily.data.1.summary');
+	}.property('results.@each')
 
 });
